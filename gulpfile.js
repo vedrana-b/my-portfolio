4bad7368ca058314deebdc4ad63ac594722b97df
vedrana <vedrana93@gmail.com>
2020-01-21 21:53:12 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+
+
+
+
+const nodeModulesPath = './node_modules';
+const jqueryPath = '/jquery/dist/jquery.min.js';
+//const foundationPath = '/foundation-sites/dist/css/foundation.min.css';
+
+/*
+--TOP LEVEL FUNCTIONS
+gulp.tasks - Define tasks
+gulp.src - Point to files to use
+gulp.dest - Point to the folder to output,
+gulp.watch - Watch files and folders for changes
+
+*/
+
+
+// Copy All Html files
+gulp.task('copyHtml', function () {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+// Compile Sass
+gulp.task('sass', function () {
+    gulp.src('src/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Copy external libraries:
+// jquery, foundation
+/*gulp.task('copyExternalLibraries', function () {
+    // jquery
+    gulp.src(nodeModulesPath + jqueryPath)
+        .pipe(gulp.dest('dist/js'));
+    // foundation
+});*/
+
+// Copy All Images
+gulp.task('copyImages', function () {
+    gulp.src('src/img/**')
+        .pipe(gulp.dest('dist/img'));
+});
+
+// Copy All Fonts
+gulp.task('copyFonts', function () {
+    gulp.src('src/font/**')
+        .pipe(gulp.dest('dist/css/font'));
+});
+
+// Minify Js
+gulp.task('minify', function () {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/*.html', ['copyHtml']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    //gulp.watch('node_modules/*', ['copyExternalLibraries'])
+    gulp.watch('src/img/**/*', ['copyImages']);
+    gulp.watch('src/font/**/*', ['copyFonts']);
+    gulp.watch('src/js/*.js', ['minify']);
+});
+
+gulp.task('package', ['copyHtml', 'copyImages', 'copyFonts', 'sass', 'minify']);
+
+gulp.task('default', ['copyHtml', 'copyImages', 'copyFonts', 'sass', 'minify', 'watch']);
+
