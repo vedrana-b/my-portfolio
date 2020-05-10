e78053355706dbb573391d7b79f623298a725964
vedrana <vedrana93@gmail.com>
2020-05-10 22:53:22 +0200

    fix mobile view and footer

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,39 +1,43 @@
-$(".js-hamburger-menu").click(function (e) {
-    $("body").toggleClass('hamburger-show hamburger-hide');
+$(".js-hamburger-menu").click(function(e) {
+  $("body").toggleClass("hamburger-show hamburger-hide");
 });
 
+if ($(window).width() < 960) {
+  $(".main").removeClass("animation");
+} else {
+  $(".main").addClass("animation");
+}
 
 gsap.registerPlugin(MotionPathPlugin);
 
 const tween = gsap.timeline();
 tween.to(".paper-plane", {
-    duration: 1,
-    ease: "power1.ease",
-    motionPath: {
-        path: [
-            { x: 100, y: -20 },
-            { x: 300, y: 10 },
-            { x: 500, y: 100 },
-            { x: 750, y: -100 },
-            { x: 350, y: -50 },
-            { x: 600, y: 100 },
-            { x: 800, y: 0 },
-            { x: window.innerWidth, y: -250 }
-        ], // you probably want more points here...or just use an SVG <path>!
-        curviness: 1.25,
-        autoRotate: false
-    }
+  duration: 1,
+  ease: "power1.ease",
+  motionPath: {
+    path: [
+      { x: 100, y: -20 },
+      { x: 300, y: 10 },
+      { x: 500, y: 100 },
+      { x: 750, y: -100 },
+      { x: 350, y: -50 },
+      { x: 600, y: 100 },
+      { x: 800, y: 0 },
+      { x: window.innerWidth, y: -250 }
+    ], // you probably want more points here...or just use an SVG <path>!
+    curviness: 1.25,
+    autoRotate: false
+  }
 });
-
 
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: '.animation',
-    duration: 3000,
-    triggerHook: 0
+  triggerElement: ".animation",
+  duration: 3000,
+  triggerHook: 0
 })
-    .setTween(tween)
-    //.addIndicators()
-    .setPin('.animation')
-    .addTo(controller);
+  .setTween(tween)
+  //.addIndicators()
+  .setPin(".animation")
+  .addTo(controller);
